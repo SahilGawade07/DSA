f598cb17f0d4b895e705e7c7409741b7c2974f4b
Sahil1459 <sahilgawade712@gmail.com>
2025-06-22 23:20:38 +0530

    solved majority element 169

--- majorityElement_169.java
+++ majorityElement_169.java
@@ -1,35 +1,3 @@
-// import java.util.HashMap;
-// import java.util.Map;
-
-// public class majorityElement_169 {
-//     public static void main(String[] args) {
-//         int nums[] = { 2, 2, 1, 1, 1, 2, 2 };
-//         System.out.println(fn(nums));
-//     }
-
-//     public static int fn(int nums[]) {
-//         HashMap<Integer, Integer> hm = new HashMap<>();
-//         int max = 0;
-//         int maxKey = 0;
-
-//         for (int i : nums) {
-//             if (hm.containsKey(i)) {
-//                 hm.put(i, hm.get(i) + 1);
-//             } else {
-//                 hm.put(i, 1);
-//             }
-//         }
-
-//         for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-//             if (entry.getValue() > max) {
-//                 max = entry.getValue();
-//                 maxKey = entry.getKey();
-//             }
-//         }
-//         return maxKey;
-//     }
-// }
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -40,25 +8,25 @@ public class majorityElement_169 {
     }
 
     public static int fn(int nums[]) {
-        int candidate = 0;
-        int count = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int max = 0;
+        int maxKey = 0;
 
-        for(int i : nums){
-        candidate = i;
-        if()
+        for (int i : nums) {
+            if (hm.containsKey(i)) {
+                hm.put(i, hm.get(i) + 1);
+            } else {
+                hm.put(i, 1);
+            }
         }
 
-        // for (int i = 1; i < nums.length; i++) {
-        //     if (nums[i] == candidate) {
-        //         count++;
-        //     } else if (nums[i] != candidate && count != 0) {
-        //         candidate = nums[i];
-        //         count = 1;
-        //     } else {
-        //         candidate = nums[i];
-        //         count = 1;
-        //     }
-        // }
-        return candidate;
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                maxKey = entry.getKey();
+            }
+        }
+
+        return maxKey;
     }
 }
