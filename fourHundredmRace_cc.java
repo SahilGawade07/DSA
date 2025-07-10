e92fca27db8e0c92f3f06fa715df7be7d9a80f5f
Sahil1459 <sahilgawade712@gmail.com>
2025-07-10 12:38:07 +0530

    solved 4000mRace from cc

---
+++ fourHundredmRace_cc.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class fourHundredmRace_cc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            int Z = sc.nextInt();
+
+            int avgX = 400 / X;
+            int avgY = 400 / Y;
+            int avgZ = 400 / Z;
+
+            if (avgX > avgY && avgX > avgZ) {
+                System.out.println("ALICE");
+            } else if (avgY > avgX && avgY > avgZ) {
+                System.out.println("BOB");
+            } else {
+                System.out.println("CHARLIE");
+            }
+        }
+        sc.close();
+    }
+}
