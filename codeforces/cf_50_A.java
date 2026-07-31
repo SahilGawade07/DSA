92a941aa47383baca81c1dbfff057c9d07d60eee
Sahil Gawade <sahilgawa07@gmail.com>
2026-07-31 13:48:58 +0530

    add cf_50_A.java to implement logic for calculating half of the area based on user input dimensions

---
+++ codeforces/cf_50_A.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class cf_50_A {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m  = sc.nextInt();
+        int n = sc.nextInt();
+        
+        int area = m*n;
+
+        System.out.println(area/2);
+    
+    }
+}
